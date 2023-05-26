50e121aacdf16b2d092a93f00297a2a2cc14fcac
wannabe <wannabewastaken@gmail.com>
2023-05-26 10:39:34 +0700

    update v1.1

--- main.sh
+++ main.sh
@@ -1,56 +1,162 @@
-#!/bin/bash
+#!/usr/bin/bash
+# admin-finder: A script used to find the admin login page of a website.
+# https://github.com/wannabewastaken/admin-finder
+#-----------------------------------------------------------------------
 
-grey=$(printf '\x1b[1;30m'); red=$(printf '\x1b[1;31m'); green=$(printf '\x1b[1;32m')
-yellow=$(printf '\x1b[1;33m'); default=$(printf '\x1b[1;0m')
+version=1.1
 
-function banner() {
-    echo "+-------------------+"
-    echo "${default}|    ${grey}adminFinder${default}    |"
-    echo -e "+-------------------+\n"
+# Speed up script by not using unicode
+LC_ALL=C
+LANG=C
+
+# Color definition
+: "${orange:=\e[38;5;202m}"
+: "${red:=\e[38;5;160m}"
+: "${yellow:=\e[38;5;220m}"
+: "${green:=\e[38;5;40m}"
+: "${reset:=\e[m}"
+
+# Checking dependecies
+command -v curl &> /dev/null || { printf "%b\n" \
+     "${red}curl ${reset}not installed."; exit 1; }
+command -v cut &> /dev/null || { printf "%b\n" \
+     "${red}cut ${reset}not installed."; exit 1; }
+
+banner() { printf "%b" "\
+${orange}▄▀█  ${reset}admin-finder
+${orange}█▀█  ${reset}132405a (${green}latest${reset})\n\n"
 }
 
-function main() {
-    site=$1; regex="[a-zA-Z0-9]+\.[a-zA-Z0-9]+.*"
-    wordlist=$(echo "Li93b3JkbGlzdC9saXN0LnR4dAo=" | base64 -d)
-    if [[ $site =~ $regex ]]; then
-        site_="$(echo ${BASH_REMATCH[0]} | cut -d '/' -f 1)"
-        if [[ $(curl -s -o /dev/null "https://${site_}" -w "%{http_code}") != "200" ]]; then
-            if [[ $(curl -s -o /dev/null "http://${site_}" -w "%{http_code}") != "200" ]]; then
-                echo "[${red}?${default}] Unknown protocol, maybe down or bad url."
-                exit 0 > /dev/null 2>&1
-            else
-                protocol="http://"; echo "[${yellow}#${default}] Your site ${site_} is stable!"
-            fi
+usage() { printf "%s" "\
+Usage:
+    $0 [option] [value]
+Options:
+    -h, --help              Show this help message and exit
+    -u, --url <url>         Target url (e.g. 'www.example.com' or 'example.com')
+    -t, --thread <thread>   Set threads number (default: 100)
+    -w, --wordlist <file>   Use custom wordlist
+
+"
+exit 1
+}
+
+scan_protocol() {
+    if [[ $(curl -s "https://${1}" \
+                 -w "%{http_code}" \
+                 -o /dev/null) =~ 200 ]]; then
+        protocol="https://"
+    else
+        if [[ $(curl -s "http://${1}" \
+                     -w "%{http_code}" \
+                     -o /dev/null) =~ 200 ]]; then
+            protocol="http://"
         else
-            protocol="https://"; echo "[${yellow}#${default}] Your site ${site_} is stable!"
+            printf "%b\n" \
+            "[$(date +%H:%M:%S)] Connection failed, aborted"
+            printf "%b\n\n" \
+            "[$(date +%H:%M:%S)] Please check your internet connection or maybe website is down"
+            exit 1
         fi
-    else
-        echo "[${red}!${default}] That's not url anymore kiddie."
-        exit 0 > /dev/null 2>&1
-    fi; echo -e "[${yellow}#${default}] Wordlist: $(wc -l < ${wordlist})"; scan; exit 0 > /dev/null 2>&1
+    fi
 }
 
-function scan() {
-    x=1; while read -r path; do
-        url="${protocol}${site_}/${path}"; status=$(curl -s -o /dev/null "${url}" -w "%{http_code}")
-        if [[ "${status}" == "200" ]] || [[ "${status}" == "201" ]]; then
-            _o=1; break; exit 0 > /dev/null 2>&1
-        else
-            echo -ne "\r[${yellow}SCANNING${default}]";
-        fi; ((x++))
-    done < <(grep "" $wordlist)
-    if [[ $_o -gt 0 ]]; then
-        echo -ne "\r[${green}FOUND${default}] ${url}\n"
-    else
-        echo -ne "\r[${red}NOTFOUND${default}] Try to find it by yourself.\n"
-    fi; exit 0 >/dev/null 2>&1
+scan_robots() {
+    for robotPath in "robot.txt" "robots.txt"; do
+        if [[ $(curl -s "${1}${2}/${robotPath}" \
+                     -w "%{http_code}" \
+                     --connect-timeout 1 \
+                     -o /dev/null) =~ 200 ]]; then
+            printf "%s\n" \
+            " |-- robots.txt -> ${1}${2}/${robotPath}"
+            isFound=true
+            break
+        fi
+    done
+    [[ ${isFound} ]] && return || {
+        printf "%s\n" \
+        " |-- robots.txt -> not found"
+    }
 }
 
+scan_path() {
+    if [[ $(curl -s "${1}${2}/${3}" -w "%{http_code}" -o /dev/null) -eq 200 ]]; then
+        printf "%b" \
+        "\e[J |-- (${green}200${reset}) -> ${1}${2}/${3}\n"
+    else
+        printf "%b" \
+        "\e[K |-- (${orange}checking${reset}) -> ${1}${2}/${3}${reset}\r"
+    fi
+}
 
-## Start
-if [[ ${#@} != 1 ]]; then
-    clear; echo "Usage: $0 <url>"
-    exit 0 > /dev/null 2>&1
-else
-    clear; banner; main $@
-fi
+main() {
+    if [[ -z $@ ]]; then
+        usage
+    fi
+
+    while [[ "$#" -gt 0 ]]; do
+        case $1 in
+            -h|--help)
+                usage
+                shift
+            ;;
+            -u|--url)
+                url="$2"
+                shift
+            ;;
+            -t|--thread)
+                thread="$2"
+                shift
+            ;;
+            -w|--wordlist)
+                wordlist="$2"
+                shift
+            ;;
+            *)
+                printf "%s\n%s\n" "admin-finder: unrecognize option '$1'" \
+                       "See the output of $0 -h for summary options."
+                exit 1
+            ;;
+        esac
+        shift
+    done
+
+    if [[ "$url" =~ [^https?://].* ]]; then
+        url=$(printf $url | cut -d "/" -f 3)
+    fi
+
+    if [[ -z "$thread" ]]; then
+        thread=100
+    fi
+
+    if [[ -z "$wordlist" ]]; then
+        wordlist="./wordlist.txt"
+    fi
+
+    clear
+    banner
+
+    printf "%s\n" \
+    "[$(date +%H:%M:%S)] Starting connection to ${url}"
+    scan_protocol ${url}
+    printf "%s\n" \
+    "[$(date +%H:%M:%S)] Connection successful"
+    printf "%s\n" \
+    "[$(date +%H:%M:%S)] Using protocol (${protocol})"
+    printf "%s\n" \
+    "[$(date +%H:%M:%S)] Scanning..."
+    scan_robots ${protocol} ${url}
+
+    while read -r path; do
+        ((x=x%${thread})); ((x++==0)) && wait
+            scan_path ${protocol} ${url} ${path} &
+    done < <(grep "" ${wordlist})
+    wait
+
+    printf "%b\n" \
+    "\e[K[$(date +%H:%M:%S)] Disconnect from ${url}"
+    printf "%s\n\n" \
+    "[$(date +%H:%M:%S)] Scanning done, exit"
+}
+
+main "$@"
+wait
